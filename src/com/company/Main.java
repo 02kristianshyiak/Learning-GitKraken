f55d943a33c678b730c80ed324fd157fa61fcece
Kristian <02kristian.shyiak@gulllakecs.org>
2019-11-12 09:53:10 -0500

    Add weather function

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,16 @@
 package com.company;
 
 public class Main {
+    public static void helloWorld() {
+        System.out.println("Hello World!");
+    }
+
+    public static void weather() {
+        System.out.println("Its snowing!");
+    }
 
     public static void main(String[] args) {
-	// write your code here
+        helloWorld();
+        weather();
     }
 }
