24a5d95c42f84fb6bb8d801fd40c698f182709d2
Kristian <02kristian.shyiak@gulllakecs.org>
2019-11-13 10:00:05 -0500

    Master Fixed

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,11 +1,12 @@
 package com.company;
 
 public class Main {
-	public static void sports() {
+    public static void sports() {
         System.out.println("You play hockey!");
     }
 
     public static void main(String[] args) {
-	sports();
+        sports();
     }
 }
+
